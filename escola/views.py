262115516c5492d325f54b60b1198fb85ccd6105
TaisLuiza <taissalissousa@gmail.com>
2026-09-21 20:20:38 -0300

    iniciando o projeto com os arquivos do Django Rest -1

---
+++ escola/views.py
@@ -0,0 +1,39 @@
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasCursoSerializer, ListaMatriculasEstudanteSerializer
+from rest_framework import viewsets, generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class EstudanteViewSet(viewsets.ModelViewSet):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated] #PARA MODIFICIAR O TIPO DE AUTENTICAÇÃO É SÓ MUDAR DENTRO DE CHAVES O TIPO
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+
+# PARA SELECIONAR APENAS UMA MATRICA DE UM ESTUDANTE
+class ListaMatriculaEstudante(generics.ListAPIView):
+    
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])  # FILTRA PARA PEGAR APENAS 1, PELA CHAVE PRIMARIA
+        return queryset
+    serializer_class = ListaMatriculasEstudanteSerializer
+
+
+class ListaMatriculaCurso(generics.ListAPIView):
+   
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasCursoSerializer
