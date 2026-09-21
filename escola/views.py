6282cba44442e2657002816ea0dc4b8c71bf2fb0
TaisLuiza <taissalissousa@gmail.com>
2026-09-21 20:40:07 -0300

    ordenando as rotas pelo id

--- escola/views.py
+++ escola/views.py
@@ -6,19 +6,18 @@ from rest_framework.permissions import IsAuthenticated
 
 
 class EstudanteViewSet(viewsets.ModelViewSet):
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated] #PARA MODIFICIAR O TIPO DE AUTENTICAÇÃO É SÓ MUDAR DENTRO DE CHAVES O TIPO
-    queryset = Estudante.objects.all()
+
+    queryset = Estudante.objects.all().order_by("id")
     serializer_class = EstudanteSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
     
-    queryset = Curso.objects.all()
+    queryset = Curso.objects.all().order_by("id")
     serializer_class = CursoSerializer
 
 class MatriculaViewSet(viewsets.ModelViewSet):
     
-    queryset = Matricula.objects.all()
+    queryset = Matricula.objects.all().order_by("id")
     serializer_class = MatriculaSerializer
 
 
@@ -26,7 +25,8 @@ class MatriculaViewSet(viewsets.ModelViewSet):
 class ListaMatriculaEstudante(generics.ListAPIView):
     
     def get_queryset(self):
-        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])  # FILTRA PARA PEGAR APENAS 1, PELA CHAVE PRIMARIA
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk']).order_by("id")
+          # FILTRA PARA PEGAR APENAS 1, PELA CHAVE PRIMARIA
         return queryset
     serializer_class = ListaMatriculasEstudanteSerializer
 
@@ -34,6 +34,6 @@ class ListaMatriculaEstudante(generics.ListAPIView):
 class ListaMatriculaCurso(generics.ListAPIView):
    
     def get_queryset(self):
-        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk']).order_by("id")
         return queryset
     serializer_class = ListaMatriculasCursoSerializer
